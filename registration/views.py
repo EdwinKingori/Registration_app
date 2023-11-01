769e96b70f9c41e522b23c60571f3a93d6d14d09
EDDY <eddmunyiri@gmail.com>
2023-11-01 20:43:10 +0300

    Form creation

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 
 
+class RegForm(forms.Form):
+    company_name = forms.CharField(label="Company_Name")
+    number_attending = forms.IntegerField(label="N.O Attending")
+    date = forms.DateField(label="Date")
+
+
 def index(request):
     return render(request, 'registration/index.html')
+
+
+def add(request):
+    return render(request, "registration/add.html", {
+        "form": RegForm
+    })
